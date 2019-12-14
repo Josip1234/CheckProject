fe3235118148e306feec65b1c91b80a8f8462514
Josip Bošnjak <jbosnjak@unipu.hr>
2019-12-14 15:22:01 +0100

    added project

---
+++ CheckProject/src/main/java/com/check/project/CheckProjectApplication.java
@@ -0,0 +1,13 @@
+package com.check.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckProjectApplication.class, args);
+	}
+
+}
